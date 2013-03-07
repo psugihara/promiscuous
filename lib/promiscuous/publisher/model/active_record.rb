55f804f1c9e4110ce11545ac4c00f66dbaef7f43
Peter Sugihara <peter@campsh.com>
2013-03-06 19:35:43 -0500

    Squashed commit of the following:
    commit 982f2df2c2817cb8e70cec1e5196448916eb4a1b
    Author: Peter Sugihara <peter@campsh.com>
    Date:   Wed Mar 6 19:34:59 2013 -0500
    
        cleanup
    
    commit 8d6706e7b192e526e439df779013a19c144ba4b5
    Author: Peter Sugihara <peter@campsh.com>
    Date:   Wed Mar 6 19:26:47 2013 -0500
    
        tests check ActiveRecord, added start of ActiveRecord dependency tracking
    
    commit a2c9176aa2ad78f0a33cb0c07524ec3a808cc2f6
    Merge: fbdf909 0f1b624
    Author: Peter Sugihara <peter@campsh.com>
    Date:   Tue Mar 5 13:28:27 2013 -0500
    
        Merge branch 'master' of git://github.com/crowdtap/promiscuous into active_record
    
    commit 0f1b624860743c9b389192c27bc0749428088f6b
    Author: Nicolas Viennot <nicolas@viennot.biz>
    Date:   Mon Mar 4 02:12:01 2013 -0500
    
        Bunny hijack network failure routines to notify the user
    
    commit fbdf9097614a2d353740ff813382271dbd86b06a
    Author: Peter Sugihara <peter@campsh.com>
    Date:   Sun Mar 3 19:33:31 2013 -0500
    
        reads using AdciveRecord::find are tracked

--- lib/promiscuous/publisher/model/active_record.rb
+++ lib/promiscuous/publisher/model/active_record.rb
@@ -2,12 +2,13 @@ module Promiscuous::Publisher::Model::ActiveRecord
   extend ActiveSupport::Concern
   include Promiscuous::Publisher::Model::Base
 
-  # TODO FIXME This needs some serious work. We need to hook deeper.
+  require 'promiscuous/publisher/operation/active_record'
 
   included do
     around_create  { |&block| promiscuous.sync(:operation => :create,  &block) }
     around_update  { |&block| promiscuous.sync(:operation => :update,  &block) }
     around_destroy { |&block| promiscuous.sync(:operation => :destroy, &block) }
+    after_find     { |&block| promiscuous.sync(:operation => :read, &block) }
   end
 
   module ClassMethods
--- spec/integration/operation_spec.rb
+++ spec/integration/operation_spec.rb
@@ -1,249 +1,247 @@
 require 'spec_helper'
 
-if ORM.has(:mongoid)
-  describe Promiscuous do
-    before { use_fake_backend }
-    before { load_models }
-    before { run_subscriber_worker! }
+describe Promiscuous do
+  before { use_fake_backend }
+  before { load_models }
+  before { run_subscriber_worker! }
 
-    context 'when using multi reads' do
-      it 'publishes proper dependencies' do
-        pub = nil
-        Promiscuous.transaction do
-          pub = PublisherModel.create
-          PublisherModel.first
-          PublisherModel.first
-          pub.update_attributes(:field_1 => 123)
-          PublisherModel.first
-          PublisherModel.first
-          pub.update_attributes(:field_1 => 456)
-        end
-
-        dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
-        dep['link'].should  == nil
-        dep['read'].should  == nil
-        dep['write'].should == ["publisher_models:id:#{pub.id}:1"]
-
-        dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
-        dep['link'].should  == "publisher_models:id:#{pub.id}:1"
-        dep['read'].should  == ["publisher_models:id:#{pub.id}:1",
-                                "publisher_models:id:#{pub.id}:1"]
-        dep['write'].should == ["publisher_models:id:#{pub.id}:4"]
-
-        dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
-        dep['link'].should  == "publisher_models:id:#{pub.id}:4"
-        dep['read'].should  == ["publisher_models:id:#{pub.id}:4",
-                                "publisher_models:id:#{pub.id}:4"]
-        dep['write'].should == ["publisher_models:id:#{pub.id}:7"]
+  context 'when using multi reads' do
+    it 'publishes proper dependencies' do
+      pub = nil
+      Promiscuous.transaction do
+        pub = PublisherModel.create
+        PublisherModel.first
+        PublisherModel.first
+        pub.update_attributes(:field_1 => 123)
+        PublisherModel.first
+        PublisherModel.first
+        pub.update_attributes(:field_1 => 456)
       end
+
+      dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
+      dep['link'].should  == nil
+      dep['read'].should  == nil
+      dep['write'].should == ["publisher_models:id:#{pub.id}:1"]
+
+      dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
+      dep['link'].should  == "publisher_models:id:#{pub.id}:1"
+      dep['read'].should  == ["publisher_models:id:#{pub.id}:1",
+                              "publisher_models:id:#{pub.id}:1"]
+      dep['write'].should == ["publisher_models:id:#{pub.id}:4"]
+
+      dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
+      dep['link'].should  == "publisher_models:id:#{pub.id}:4"
+      dep['read'].should  == ["publisher_models:id:#{pub.id}:4",
+                              "publisher_models:id:#{pub.id}:4"]
+      dep['write'].should == ["publisher_models:id:#{pub.id}:7"]
     end
+  end
 
-    context 'when using only reads' do
-      it 'publishes proper dependencies' do
-        pub = without_promiscuous { PublisherModel.create }
-        Promiscuous.transaction(:active => true) do
-          PublisherModel.first
-        end
-
-        payload = Promiscuous::AMQP::Fake.get_next_payload
-        payload['__amqp__'].should == "__promiscuous__/dummy"
-        payload['operation'].should == "dummy"
-        dep = payload['dependencies']
-        dep['link'].should  == nil
-        dep['read'].should  == ["publisher_models:id:#{pub.id}:0"]
-        dep['write'].should == nil
+  context 'when using only reads' do
+    it 'publishes proper dependencies' do
+      pub = without_promiscuous { PublisherModel.create }
+      Promiscuous.transaction(:active => true) do
+        PublisherModel.first
       end
+
+      payload = Promiscuous::AMQP::Fake.get_next_payload
+      payload['__amqp__'].should == "__promiscuous__/dummy"
+      payload['operation'].should == "dummy"
+      dep = payload['dependencies']
+      dep['link'].should  == nil
+      dep['read'].should  == ["publisher_models:id:#{pub.id}:0"]
+      dep['write'].should == nil
     end
+  end
 
-    context 'when using multi reads/writes on tracked attributes' do
-      it 'publishes proper dependencies' do
-        PublisherModel.track_dependencies_of :field_1
-        PublisherModel.track_dependencies_of :field_2
+  context 'when using multi reads/writes on tracked attributes' do
+    it 'publishes proper dependencies' do
+      PublisherModel.track_dependencies_of :field_1
+      PublisherModel.track_dependencies_of :field_2
 
-        pub = nil
-        Promiscuous.transaction do
-          pub = PublisherModel.create(:field_1 => 123, :field_2 => 456)
-          PublisherModel.where(:field_1 => 123).count
-          PublisherModel.where(:field_1 => 'blah').count
-          PublisherModel.where(:field_1 => 123, :field_2 => 456).count
-          PublisherModel.where(:field_1 => 'blah', :field_2 => 456).count
-          PublisherModel.where(:field_2 => 456).count
-          PublisherModel.where(:field_2 => 'blah').count
-          PublisherModel.where(:field_2 => 456).first
-          pub.update_attributes(:field_1 => 'blah')
-          PublisherModel.where(:field_1 => 123).first
-          pub.update_attributes(:field_2 => 'blah')
-        end
-
-        dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
-        dep['link'].should  == nil
-        dep['read'].should  == nil
-        dep['write'].should == ["publisher_models:id:#{pub.id}:1",
-                                "publisher_models:field_1:123:1",
-                                "publisher_models:field_2:456:1"]
-
-        dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
-        dep['link'].should  == "publisher_models:id:#{pub.id}:1"
-        dep['read'].should  == ["publisher_models:field_1:123:1",
-                                "publisher_models:field_1:blah:0",
-                                "publisher_models:field_1:123:1",
-                                "publisher_models:field_1:blah:0",
-                                "publisher_models:field_2:456:1",
-                                "publisher_models:field_2:blah:0",
-                                "publisher_models:id:#{pub.id}:1"]
-        dep['write'].should == ["publisher_models:id:#{pub.id}:3",
-                                "publisher_models:field_1:123:4",
-                                # FIXME "publisher_models:field_1:blah:3",
-                                "publisher_models:field_2:456:3"]
-
-        dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
-        dep['link'].should  == "publisher_models:id:#{pub.id}:3"
-        dep['read'].should  == nil
-        dep['write'].should == ["publisher_models:id:#{pub.id}:4",
-                                "publisher_models:field_1:blah:3",
-                                "publisher_models:field_2:456:4"]
-                                # FIXME "publisher_models:field_2:blah:1",
+      pub = nil
+      Promiscuous.transaction do
+        pub = PublisherModel.create(:field_1 => 123, :field_2 => 456)
+        PublisherModel.where(:field_1 => 123).count
+        PublisherModel.where(:field_1 => 'blah').count
+        PublisherModel.where(:field_1 => 123, :field_2 => 456).count
+        PublisherModel.where(:field_1 => 'blah', :field_2 => 456).count
+        PublisherModel.where(:field_2 => 456).count
+        PublisherModel.where(:field_2 => 'blah').count
+        PublisherModel.where(:field_2 => 456).first
+        pub.update_attributes(:field_1 => 'blah')
+        PublisherModel.where(:field_1 => 123).first
+        pub.update_attributes(:field_2 => 'blah')
       end
+
+      dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
+      dep['link'].should  == nil
+      dep['read'].should  == nil
+      dep['write'].should == ["publisher_models:id:#{pub.id}:1",
+                              "publisher_models:field_1:123:1",
+                              "publisher_models:field_2:456:1"]
+
+      dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
+      dep['link'].should  == "publisher_models:id:#{pub.id}:1"
+      dep['read'].should  == ["publisher_models:field_1:123:1",
+                              "publisher_models:field_1:blah:0",
+                              "publisher_models:field_1:123:1",
+                              "publisher_models:field_1:blah:0",
+                              "publisher_models:field_2:456:1",
+                              "publisher_models:field_2:blah:0",
+                              "publisher_models:id:#{pub.id}:1"]
+      dep['write'].should == ["publisher_models:id:#{pub.id}:3",
+                              "publisher_models:field_1:123:4",
+                              # FIXME "publisher_models:field_1:blah:3",
+                              "publisher_models:field_2:456:3"]
+
+      dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
+      dep['link'].should  == "publisher_models:id:#{pub.id}:3"
+      dep['read'].should  == nil
+      dep['write'].should == ["publisher_models:id:#{pub.id}:4",
+                              "publisher_models:field_1:blah:3",
+                              "publisher_models:field_2:456:4"]
+      # FIXME "publisher_models:field_2:blah:1",
     end
+  end
 
-    context 'when using each' do
-      it 'publishes proper dependencies' do
-        PublisherModel.track_dependencies_of :field_1
+  context 'when using each' do
+    it 'publishes proper dependencies' do
+      PublisherModel.track_dependencies_of :field_1
 
-        pub1 = pub2 = nil
-        Promiscuous.transaction do
-          pub1 = PublisherModel.create(:field_1 => 123)
-          pub2 = PublisherModel.create(:field_1 => 123)
-          PublisherModel.where(:field_1 => 123).each.to_a
-          pub1.update_attributes(:field_2 => 456)
-        end
-
-        dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
-        dep['link'].should  == nil
-        dep['read'].should  == nil
-        dep['write'].should == ["publisher_models:id:#{pub1.id}:1",
-                                "publisher_models:field_1:123:1"]
-
-        dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
-        dep['link'].should  == "publisher_models:id:#{pub1.id}:1"
-        dep['read'].should  == nil
-        dep['write'].should == ["publisher_models:id:#{pub2.id}:1",
-                                "publisher_models:field_1:123:2"]
-
-        dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
-        dep['link'].should  == "publisher_models:id:#{pub2.id}:1"
-        dep['read'].should  == ["publisher_models:field_1:123:2"]
-        dep['write'].should == ["publisher_models:id:#{pub1.id}:2",
-                                "publisher_models:field_1:123:4"]
+      pub1 = pub2 = nil
+      Promiscuous.transaction do
+        pub1 = PublisherModel.create(:field_1 => 123)
+        pub2 = PublisherModel.create(:field_1 => 123)
+        PublisherModel.where(:field_1 => 123).each.to_a
+        pub1.update_attributes(:field_2 => 456)
       end
+
+      dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
+      dep['link'].should  == nil
+      dep['read'].should  == nil
+      dep['write'].should == ["publisher_models:id:#{pub1.id}:1",
+                              "publisher_models:field_1:123:1"]
+
+      dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
+      dep['link'].should  == "publisher_models:id:#{pub1.id}:1"
+      dep['read'].should  == nil
+      dep['write'].should == ["publisher_models:id:#{pub2.id}:1",
+                              "publisher_models:field_1:123:2"]
+
+      dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
+      dep['link'].should  == "publisher_models:id:#{pub2.id}:1"
+      dep['read'].should  == ["publisher_models:field_1:123:2"]
+      dep['write'].should == ["publisher_models:id:#{pub1.id}:2",
+                              "publisher_models:field_1:123:4"]
     end
+  end
 
-    context 'when using a uniqueness validator' do
-      it 'skips the query' do
-        PublisherModel.validates_uniqueness_of :field_1
+  context 'when using a uniqueness validator' do
+    it 'skips the query' do
+      PublisherModel.validates_uniqueness_of :field_1
 
-        pub = nil
-        Promiscuous.transaction do
-          pub = PublisherModel.create(:field => 123)
-        end
-
-        dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
-        dep['link'].should  == nil
-        dep['read'].should  == nil
-        dep['write'].should == ["publisher_models:id:#{pub.id}:1"]
+      pub = nil
+      Promiscuous.transaction do
+        pub = PublisherModel.create(:field => 123)
       end
-    end
 
-    context 'when using limit(1).each' do
-      it 'skips the query' do
-        pub1 = pub2 = nil
-        Promiscuous.transaction do
-          pub1 = PublisherModel.create(:field_1 => 123)
-          PublisherModel.all.limit(1).each do |pub|
-            pub.id.should      == pub1.id
-            pub.field_1.should == pub1.field_1
+      dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
+      dep['link'].should  == nil
+      dep['read'].should  == nil
+      dep['write'].should == ["publisher_models:id:#{pub.id}:1"]
+    end
+  end
+
+  context 'when using limit(1).each' do
+    it 'skips the query' do
+      pub1 = pub2 = nil
+      Promiscuous.transaction do
+        pub1 = PublisherModel.create(:field_1 => 123)
+        PublisherModel.all.limit(1).each do |pub|
+          pub.id.should      == pub1.id
+          pub.field_1.should == pub1.field_1
+        end
+        pub2 = PublisherModel.create(:field_1 => 123)
+      end
+
+      dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
+      dep['link'].should  == nil
+      dep['read'].should  == nil
+      dep['write'].should == ["publisher_models:id:#{pub1.id}:1"]
+
+      dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
+      dep['link'].should  == "publisher_models:id:#{pub1.id}:1"
+      dep['read'].should  == ["publisher_models:id:#{pub1.id}:1"]
+      dep['write'].should == ["publisher_models:id:#{pub2.id}:1"]
+    end
+  end
+
+  context 'when updating a field that is not published' do
+    it "doesn't track the write" do
+      PublisherModel.field :not_published
+
+      pub = nil
+      Promiscuous.transaction do
+        pub = PublisherModel.create
+        pub.update_attributes(:not_published => 'hello')
+        pub.update_attributes(:field_1 => 'ohai')
+      end
+
+      dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
+      dep['link'].should  == nil
+      dep['read'].should  == nil
+      dep['write'].should == ["publisher_models:id:#{pub.id}:1"]
+
+      dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
+      dep['link'].should  == "publisher_models:id:#{pub.id}:1"
+      dep['read'].should  == nil
+      dep['write'].should == ["publisher_models:id:#{pub.id}:2"]
+
+      Promiscuous::AMQP::Fake.get_next_message.should == nil
+    end
+  end
+
+  context 'when using map reduce' do
+    it 'track the read' do
+      PublisherModel.track_dependencies_of :field_1
+      without_promiscuous do
+        PublisherModel.create(:field_1 => 123)
+        PublisherModel.create(:field_1 => 123)
+      end
+      Promiscuous.transaction :active => true do
+        PublisherModel.where(:field_1 => 123).sum(:field_2)
+      end
+
+      dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
+      dep['link'].should  == nil
+      # Mongoid does an extra count.
+      dep['read'].should  == ["publisher_models:field_1:123:0",
+                              "publisher_models:field_1:123:0"]
+      dep['write'].should == nil
+    end
+  end
+
+  context 'when using without_promiscuous.each' do
+    it 'track the reads one by one' do
+      pub1 = pub2 = nil
+      without_promiscuous do
+        pub1 = PublisherModel.create(:field_1 => 123)
+        pub2 = PublisherModel.create(:field_1 => 123)
+      end
+      Promiscuous.transaction :active => true do
+        expect do
+          PublisherModel.all.without_promiscuous.where(:field_1 => 123).each do |p|
+            p.reload
           end
-          pub2 = PublisherModel.create(:field_1 => 123)
-        end
-
-        dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
-        dep['link'].should  == nil
-        dep['read'].should  == nil
-        dep['write'].should == ["publisher_models:id:#{pub1.id}:1"]
-
-        dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
-        dep['link'].should  == "publisher_models:id:#{pub1.id}:1"
-        dep['read'].should  == ["publisher_models:id:#{pub1.id}:1"]
-        dep['write'].should == ["publisher_models:id:#{pub2.id}:1"]
+        end.to_not raise_error
       end
-    end
 
-    context 'when updating a field that is not published' do
-      it "doesn't track the write" do
-        PublisherModel.field :not_published
-
-        pub = nil
-        Promiscuous.transaction do
-          pub = PublisherModel.create
-          pub.update_attributes(:not_published => 'hello')
-          pub.update_attributes(:field_1 => 'ohai')
-        end
-
-        dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
-        dep['link'].should  == nil
-        dep['read'].should  == nil
-        dep['write'].should == ["publisher_models:id:#{pub.id}:1"]
-
-        dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
-        dep['link'].should  == "publisher_models:id:#{pub.id}:1"
-        dep['read'].should  == nil
-        dep['write'].should == ["publisher_models:id:#{pub.id}:2"]
-
-        Promiscuous::AMQP::Fake.get_next_message.should == nil
-      end
-    end
-
-    context 'when using map reduce' do
-      it 'track the read' do
-        PublisherModel.track_dependencies_of :field_1
-        without_promiscuous do
-          PublisherModel.create(:field_1 => 123)
-          PublisherModel.create(:field_1 => 123)
-        end
-        Promiscuous.transaction :active => true do
-          PublisherModel.where(:field_1 => 123).sum(:field_2)
-        end
-
-        dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
-        dep['link'].should  == nil
-        # Mongoid does an extra count.
-        dep['read'].should  == ["publisher_models:field_1:123:0",
-                                "publisher_models:field_1:123:0"]
-        dep['write'].should == nil
-      end
-    end
-
-    context 'when using without_promiscuous.each' do
-      it 'track the reads one by one' do
-        pub1 = pub2 = nil
-        without_promiscuous do
-          pub1 = PublisherModel.create(:field_1 => 123)
-          pub2 = PublisherModel.create(:field_1 => 123)
-        end
-        Promiscuous.transaction :active => true do
-          expect do
-            PublisherModel.all.without_promiscuous.where(:field_1 => 123).each do |p|
-              p.reload
-            end
-          end.to_not raise_error
-        end
-
-        dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
-        dep['link'].should  == nil
-        dep['read'].should  == ["publisher_models:id:#{pub1.id}:0",
-                                "publisher_models:id:#{pub2.id}:0"]
-        dep['write'].should == nil
-      end
+      dep = Promiscuous::AMQP::Fake.get_next_payload['dependencies']
+      dep['link'].should  == nil
+      dep['read'].should  == ["publisher_models:id:#{pub1.id}:0",
+                              "publisher_models:id:#{pub2.id}:0"]
+      dep['write'].should == nil
     end
   end
 end
